fd5a6730210d9caf751236151137d0ccc8a9c94c
Brooks A. Jackson <brksjcksn03@protonmail.com>
2024-01-30 17:35:01 -0500

    Added more demos

---
+++ Demos/app/src/main/java/edu/quinnipiac/ser210/demo2/Money.kt
@@ -0,0 +1,4 @@
+package edu.quinnipiac.ser210.demo2
+
+class Money {
+}
---
+++ Demos/app/src/main/java/edu/quinnipiac/ser210/demo2/SpecifyAmountFragment.kt
@@ -0,0 +1,53 @@
+package edu.quinnipiac.ser210.ls03_navigation
+
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.os.bundleOf
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import java.math.BigDecimal
+
+
+class SpecifyAmountFragment : Fragment(), View.OnClickListener {
+
+    lateinit var navController: NavController
+    lateinit var recipient: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+     //   recipient = arguments!!.getString("recipient")
+     
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_specify_amount, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+       super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        view.findViewById<Button>(R.id.send_btn).setOnClickListener(this)
+        view.findViewById<Button>(R.id.cancel_btn).setOnClickListener(this)
+        val message = "Sending money to $recipient"
+        view.findViewById<TextView>(R.id.recipient).text = message
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id){
+        }
+    }
+}
