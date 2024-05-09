32687f225f0d481b72e2b6c1733cc1700fef517e
Brooks A. Jackson <brksjcksn03@protonmail.com>
2024-05-09 18:07:38 -0400

    Removed Assignment 2

--- Assignments/Assignment 2/app/src/main/java/edu/quinnipiac/assignment2/SharedViewModel.kt
+++
@@ -1,9 +0,0 @@
-package edu.quinnipiac.assignment2
-
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-
-class SharedViewModel : ViewModel() {
-    // MutableLiveData to hold the player's name
-    val playerName = MutableLiveData<String>()
-}
